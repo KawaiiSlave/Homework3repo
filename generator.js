30996555857d8a388675358c07777aa8f6143161
Tyler <tgandle@gmail.com>
2019-11-09 15:17:26 -0600

    Deleted extra code

--- generator.js
+++ generator.js
@@ -36,49 +36,6 @@ var all = uppercase + lowercase + numbers + symbols;
 
     }
 
-
-    
-        
-
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-var numbers = '0123456789';
-var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
-
-var all = uppercase + lowercase + numbers + symbols;
-    
-    
-
-    var length = prompt("Please enter a password length from 8-128");
-
-    var type = prompt("Would you like to use special characters?")
-
-    var password = "";
-    
-    function generatePwd() {
-        for (var i = 0; i <= length; i++) {
-        password = password + all.charAt(Math.floor(Math.random() * Math.floor(all.length-1)));
-        
-
-    }
-    
-    document.getElementById("pwd").value = password;
-
-    
-}
-
-    function copy(){
-
-        document.getElementById("pwd").select();
-
-        document.execCommand("Copy");
-
-        alert("Password copied to clipboard!");
-
-
-    }
-
-
     
         
 
